196494c99feed63199646f37f772e70107287fb9
Karinesouza06 <mickaelle2006@icloud.com>
2025-05-15 16:16:48 -0300

    aula03

--- Atividade/atividade_pos_02 - correção/models.py
+++ Atividade/atividade_pos_02 - correção/models.py
@@ -1,6 +1,24 @@
 from pydantic import BaseModel 
+from typing import List
+from datetime import date 
 
 class Livro(BaseModel):
     uuid:str
     titulo:str 
     autor:str
+    ano:str
+    disponibilidade:bool
+
+class Usuario(BaseModel):
+    uuid:str
+    nome:str
+    livros:List[Livro]
+
+class Emprestimo(BaseModel):
+    usuario:Usuario
+    livro:Livro
+    emprestimo:date 
+    devolucao:date
+
+
+
